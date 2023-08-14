ddbe7f3ba17bb6408905a5311a13bdd0896bbebd
Emman-pip <115330041+Emman-pip@users.noreply.github.com>
2023-08-14 15:45:56 +0800

    Add a somehow functioning append function.

--- script.js
+++ script.js
@@ -1,13 +1,46 @@
-const listFactory = (list = {}) => {
-  return { list };
+function toEnd(data) {
+  console.log("data", data);
+  if (data == undefined) {
+    console.log("undefined data");
+  } else if (data.next === null) {
+    console.log("data is null");
+    // console.log("data", data);
+    return data;
+  } else if (Boolean(data.next) === true) {
+    console.log("i read the next node");
+    toEnd(data.next);
+  }
+}
+
+const listFactory = () => {
+  const obj = {};
+  const append = (value) => {
+    if (Object.values(obj)[0] == undefined) {
+      obj.data = value;
+    } else {
+      const lastNode = toEnd(obj.data);
+      // console.log(Object.keys(lastNode), Object.values(lastNode));
+      if (lastNode.next === null) {
+        lastNode.next = value;
+        return;
+      }
+      console.log("error here");
+    }
+  };
+  return { obj, append };
 };
 
-const nodeFactory = (value = null, nextNode = null) => {
-  return { value, nextNode };
+const nodeFactory = (value = null, next = null) => {
+  return { value, next };
 };
 
-const lol = listFactory();
+const LinkedL = listFactory();
+// // lol.list.next = nodeFactory("tail");
+LinkedL.append(nodeFactory("first"));
+LinkedL.append(nodeFactory("second"));
+// LinkedL.append(nodeFactory("third"));
 
-lol.list.value = "haha";
-
-console.log(lol);
+console.log(LinkedL.obj);
+// console.log(LinkedL.obj.data.next);
+// LinkedL.append(nodeFactory("third"));
+// LinkedL.append(nodeFactory("fourth"));
